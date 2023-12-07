eadf88fd2a0f1966dfbc27b3896de5952e705ff4
jebibot <83044352+jebibot@users.noreply.github.com>
2023-12-07 21:14:47 +0900

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,10 @@ export const metadata: Metadata = {
   title: "10000팔",
   description:
     "트위치 팔로워 수가 10,000명에 근접한 스트리머의 방송을 소개합니다.",
-  keywords: ["Twitch", "streamer", "치지직"],
+  keywords: ["Twitch", "streamer", "치지직", "만팔", "품앗이"],
+  alternates: {
+    canonical: "https://10000.twitchgg.tv/",
+  },
 };
 
 export default function RootLayout({
